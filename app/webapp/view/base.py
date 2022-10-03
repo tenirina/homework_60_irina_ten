240838b8bb962d691b77ddb1b98413c7c62f6216
Irina Ten <virhot17@gmail.com>
2022-10-03 19:29:24 +0600

    Improvement
    Добавлена форма поиска на главную страницу

--- app/webapp/view/base.py
+++ app/webapp/view/base.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 
+from webapp.forms import SearchForm
 from webapp.models import Product
 
 
 def index_view(request):
-    products = Product.objects.filter(is_delete=False, balance__gt=1).order_by('category', 'title')
+    form = SearchForm(request.GET)
+    if 'search' in form.data:
+        if form.data['search']:
+            products = Product.objects.filter(is_delete=False, balance__gt=1, title__icontains=form.data['search']).order_by('category', 'title')
+        else:
+            products = Product.objects.filter(is_delete=False, balance__gt=1).order_by('category', 'title')
+    else:
+        products = Product.objects.filter(is_delete=False, balance__gt=1).order_by('category', 'title')
     for el in products:
         el.category = el.get_category_display()
     context = {
