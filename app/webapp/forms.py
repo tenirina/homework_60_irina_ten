762718860b719a8a8194a1ea79992744a8a1e870
Irina Ten <virhot17@gmail.com>
2022-10-03 20:29:53 +0600

    Improvment
     Добавлена форма для добавления продукта.
     Исправлено поле price. Сделаны и применены миграции и сделана выгрузка фикстур

--- app/webapp/forms.py
+++ app/webapp/forms.py
@@ -1,7 +1,15 @@
 from django import forms
+from django.forms import widgets
 
 
 class SearchForm(forms.Form):
     search = forms.CharField(max_length=100, required=False, label="Search", initial="")
 
 
+class ProductForm(forms.Form):
+    title = forms.CharField(max_length=100, required=True, label="Title")
+    description = forms.CharField(max_length=2000, required=False, label="Description",  widget=widgets.Textarea)
+    photo = forms.CharField(max_length=200, required=True, label="Photo")
+    category = forms.CharField(max_length=100, required=True, label="Category")
+    balance = forms.IntegerField(required=True, label="Balance")
+    price = forms.DecimalField(max_digits=9, decimal_places=2, required=True, label="Price")
--- app/webapp/models.py
+++ app/webapp/models.py
@@ -14,7 +14,7 @@ class Product(models.Model):
     photo = models.CharField(verbose_name="Photo", max_length=200, null=False, blank=False)
     category = models.CharField(verbose_name="Title", max_length=100, null=False, blank=False, choices=CHOICES)
     balance = models.IntegerField(verbose_name="Balance", null=False, blank=False)
-    price = models.DecimalField(verbose_name="Price", max_digits=10, decimal_places=2, null=False, blank=False)
+    price = models.DecimalField(verbose_name="Price", max_digits=9, decimal_places=2, null=False, blank=False)
     created_at = models.DateTimeField(verbose_name="Date of create", auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name="Date of create", auto_now=True, null=True)
     is_delete = models.BooleanField(verbose_name="Delete", default=False, null=False)
