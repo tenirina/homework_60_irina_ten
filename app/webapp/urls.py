53dc258f65b59386b34f2034ab8d41b4ccbc32d8
Irina Ten <virhot17@gmail.com>
2022-10-03 16:13:19 +0600

    Add
    Добавлены данные для просмотра на сраницу index.html. Настроены стили.
    
    Добавлен шаблон для html.

--- app/webapp/urls.py
+++ app/webapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from webapp.view.base import index_view
+from webapp.view.base import index_view, product_view
 
 urlpatterns = [
     path("", index_view, name='index'),
+    path("product/<int:pk>", product_view, name='product')
 ]
--- app/webapp/view/base.py
+++ app/webapp/view/base.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
+from webapp.models import Product
+
 
 def index_view(request):
-    return render(request, 'index.html')
+    products = Product.objects.all()
+    for el in products:
+        el.category = el.get_category_display()
+    context = {
+        'products': products
+    }
+    return render(request, 'index.html', context)
+
+
+def product_view(request, pk):
+    pass
