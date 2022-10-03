9627f9351f45cc081407c951a97972b95a3e97b4
Irina Ten <virhot17@gmail.com>
2022-10-03 13:40:01 +0600

    Improvement
    Модель зарегестрирована в админ-панели.

--- app/webapp/admin.py
+++ app/webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'category', 'balance', 'price', 'created_at')
+    list_filter = ('id', 'title', 'description', 'category')
+    search_fields = ('title', 'description', 'category')
+    fields = ('id', 'title', 'description', 'category', 'balance', 'price', 'created_at')
+    readonly_fields = ('id', 'title', 'category')
+
+
+admin.site.register(Product, ProductAdmin)
