b1f75882765a29ee14e22d2e639ce4ce0f482bdb
Irina Ten <virhot17@gmail.com>
2022-10-04 13:01:06 +0600

    Add
     Добавлена возможность изменять продукт.

--- app/webapp/urls.py
+++ app/webapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from webapp.view.base import index_view
-from webapp.view.products import product_view, create_view
+from webapp.view.products import product_view, create_view, edit_view
 
 urlpatterns = [
     path("", index_view, name='index'),
     path("product/<int:pk>", product_view, name='product'),
     path("create/", create_view, name="create"),
+    path("product/<int:pk>/edit", edit_view, name='edit')
 ]
--- app/webapp/view/products.py
+++ app/webapp/view/products.py
@@ -43,3 +43,33 @@ def create_view(request):
         return render(request, 'create.html', context={'choices': Product.CHOICES, 'form': form})
     product = Product.objects.create(**form.cleaned_data)
     return redirect('product', pk=product.pk)
+
+
+def edit_view(request, pk):
+    form = ProductForm()
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == "POST":
+        form = ProductForm(request.POST)
+        errors = errors_test(form)
+        if errors:
+            product = {
+                'title': form.data['title'],
+                'description': form.data['description'],
+                'photo': form.data['photo'],
+                'category': form.data['category'],
+                'balance': form.data['balance'],
+                'price': form.data['price']
+            }
+            return render(request, 'create.html',
+                          context={'product': product, 'errors': errors, 'choices': Product.CHOICES})
+        if form.is_valid():
+            product.title = form.cleaned_data['title']
+            product.description = form.cleaned_data['description']
+            product.photo = form.cleaned_data['photo']
+            product.category = form.cleaned_data['category']
+            product.balance = form.cleaned_data['balance']
+            product.price = form.cleaned_data['price']
+            product.save()
+            return redirect('index')
+    return render(request, 'edit.html', context={'product': product, 'form': form, 'choices': Product.CHOICES})
+
