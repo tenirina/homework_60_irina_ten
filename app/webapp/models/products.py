92ae7735d7087137f58af5f41b6e0154b4b27c3a
Irina Ten <virhot17@gmail.com>
2022-10-14 19:42:01 +0600

    Add
     Добавлена пагинация для отображения основной странички.
     Модель product перенесена в отдельную папку и переписана согласно классовым представлениям.

--- app/webapp/models/products.py
+++ app/webapp/models/products.py
@@ -1,19 +1,21 @@
 from django.db import models
+from django.db.models import TextChoices
 from django.utils import timezone
 
 
+class CategoryChoices(TextChoices):
+    OTHER = "other", "Other"
+    FRUITS = "fruits", "Fruits"
+    VEGETABLES = "vegetables", "Vegetables"
+    PREPARED = "prepared", "Prepared food"
+
+
 class Product(models.Model):
-    CHOICES = (
-        ("other", "Other"),
-        ("fruits", "Fruits"),
-        ("vegetables", "Vegetables"),
-        ("prepared", "Prepared food")
-    )
 
     title = models.CharField(verbose_name="Title", max_length=100, null=False, blank=False)
     description = models.TextField(verbose_name="Description", max_length=2000, null=True, blank=False)
     photo = models.CharField(verbose_name="Photo", max_length=200, null=True, blank=False)
-    category = models.CharField(verbose_name="Category", max_length=100, null=False, blank=False, choices=CHOICES)
+    category = models.CharField(verbose_name="Category", max_length=100, null=False, blank=False, choices=CategoryChoices.choices)
     balance = models.IntegerField(verbose_name="Balance", null=False, blank=False)
     price = models.DecimalField(verbose_name="Price", max_digits=9, decimal_places=2, null=False, blank=False)
     created_at = models.DateTimeField(verbose_name="Date of create", auto_now_add=True)
@@ -29,3 +31,6 @@ class Product(models.Model):
         self.deleted_at = timezone.now()
         self.is_delete = True
         self.save()
+
+
+
--- app/webapp/view/base.py
+++ app/webapp/view/base.py
@@ -1,46 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-from webapp.forms import SearchForm
 from webapp.models import Product
 
 
-def index_view(request):
-    form = SearchForm(request.GET)
-    if 'search' in form.data:
-        if form.data['search']:
-            products = Product.objects.filter(is_delete=False, balance__gt=1, title__icontains=form.data['search']).order_by('category', 'title')
-        else:
-            products = Product.objects.filter(is_delete=False, balance__gt=1).order_by('category', 'title')
-    else:
-        products = Product.objects.filter(is_delete=False, balance__gt=1).order_by('category', 'title')
-    for el in products:
-        el.category = el.get_category_display()
-    context = {
-        'products': products,
-        'choices': Product.CHOICES
-    }
-    return render(request, 'index.html', context)
+class IndexView(ListView):
+    template_name = "products/index.html"
+    model = Product
+    context_object_name = "products"
+    paginate_by = 6
+    paginate_orphans = 2
+    queryset = Product.objects.exclude(is_delete=True)
 
 
-def choice_category_view(request, category):
-    form = SearchForm(request.GET)
-    if 'search' in form.data:
-        if form.data['search']:
-            products = Product.objects.filter(is_delete=False, category=category, title__icontains=form.data['search']).order_by('title')
-        else:
-            products = Product.objects.filter(is_delete=False, category=category).order_by('title')
-    else:
-        products = Product.objects.filter(is_delete=False, category=category).order_by('title')
-    for el in products:
-        el.category = el.get_category_display()
-    for el in Product.CHOICES:
-        if el[0] == category:
-            category = el[1]
-            break
-    context = {
-        'products': products,
-        'category': category,
-        'choices': Product.CHOICES
-    }
-    return render(request, 'category.html', context)
 
